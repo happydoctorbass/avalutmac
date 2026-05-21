fd3bf50468000271ef8cfd501bcce586aa879895
happydoctorbass <nursmanov1@gmail.com>
2026-05-21 15:25:44 +0600

    Fix next config for Vercel

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: { 
-    allowedDevOrigins: ['10.15.249.108'] 
-  }
 };
 
 export default nextConfig;
