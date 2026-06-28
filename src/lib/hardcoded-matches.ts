c171c1786f837340342809fd245071ccd5733530
happydoctorbass <nursmanov1@gmail.com>
2026-06-28 15:08:15 +0600

    Final Admiral Casino System 3.0

--- src/lib/hardcoded-matches.ts
+++ src/lib/hardcoded-matches.ts
@@ -3,9 +3,13 @@ import { Match } from '@/types/match';
 export type HardcodedMatch = Match & { prize: string };
 
 const rawMatches = [
+  { date: '29.06', time: '01:00', team1: 'RSA', team2: 'CAN', prize: '5000 KGS' },
   { date: '29.06', time: '23:00', team1: 'BRA', team2: 'JPN', prize: '5000 KGS' },
   { date: '30.06', time: '02:30', team1: 'GER', team2: 'PAR', prize: '5000 KGS' },
   { date: '30.06', time: '07:00', team1: 'NED', team2: 'MAR', prize: '5000 KGS' },
+  { date: '30.06', time: '23:00', team1: 'CIV', team2: 'NOR', prize: '5000 KGS' },
+  { date: '01.07', time: '03:00', team1: 'FRA', team2: 'SWE', prize: '5000 KGS' },
+  { date: '01.07', time: '07:00', team1: 'MEX', team2: 'ECU', prize: '5000 KGS' },
 ];
 
 export const HARDCODED_MATCHES: HardcodedMatch[] = rawMatches.map((m, i) => {
