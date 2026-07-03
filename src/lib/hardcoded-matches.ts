dbe3027eb069b397099ff69f26e1c617977663a2
happydoctorbass <nursmanov1@gmail.com>
2026-07-04 01:01:54 +0600

    Final Admiral Casino System 3.0

--- src/lib/hardcoded-matches.ts
+++ src/lib/hardcoded-matches.ts
@@ -3,16 +3,11 @@ import { Match } from '@/types/match';
 export type HardcodedMatch = Match & { prize: string };
 
 const rawMatches = [
-  { date: '01.07', time: '22:00', team1: 'ENG', team2: 'COD', prize: '5000 KGS' },
-  { date: '02.07', time: '03:00', team1: 'USA', team2: 'BIH', prize: '5000 KGS' },
-  { date: '02.07', time: '02:00', team1: 'BEL', team2: 'SEN', prize: '5000 KGS' },
-  { date: '03.07', time: '05:00', team1: 'POR', team2: 'CRO', prize: '5000 KGS' },
-  { date: '03.07', time: '01:00', team1: 'ESP', team2: 'AUT', prize: '5000 KGS' },
-  { date: '03.07', time: '09:00', team1: 'SUI', team2: 'ALG', prize: '5000 KGS' },
-  { date: '04.07', time: '04:00', team1: 'ARG', team2: 'CPV', prize: '5000 KGS' },
-  { date: '04.07', time: '07:30', team1: 'COL', team2: 'GHA', prize: '5000 KGS' },
-  { date: '04.07', time: '00:00', team1: 'AUS', team2: 'EGV', prize: '5000 KGS' },
-  { date: '05.07', time: '03:00', team1: 'PAR', team2: 'FRA', prize: '5000 KGS' },
+  { date: '04.07', time: '23:00', team1: 'CAN', team2: 'MAR', prize: '5000 KGS' },
+  { date: '06.07', time: '02:00', team1: 'BRA', team2: 'NOR', prize: '5000 KGS' },
+  { date: '06.07', time: '06:00', team1: 'MEX', team2: 'ENG', prize: '5000 KGS' },
+  { date: '07.07', time: '01:00', team1: 'POR', team2: 'ESP', prize: '5000 KGS' },
+  { date: '07.07', time: '06:00', team1: 'USA', team2: 'BEL', prize: '5000 KGS' },
 ];
 
 export const HARDCODED_MATCHES: HardcodedMatch[] = rawMatches.map((m, i) => {
